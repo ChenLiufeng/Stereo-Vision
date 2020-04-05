dd0f3a7074c2d23153e20483a2bc00f4b9e6cca9
michaelboyadjian <michael.boyadjian@mail.utoronto.ca>
2020-04-05 08:28:16 -0400

    Cleaned up code

--- disparity.py
+++ disparity.py
@@ -2,13 +2,23 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def left(matrix, n):
-    out = np.zeros(matrix.shape)
-    rows, cols = matrix.shape
-    out[:, 0:cols-n] = matrix[:, n:cols]
-    return out
+def main():
+    disparity = Disparity('im0.png', 'im1.png', 1000, 1482, 11, 140)
+    PlotDepth2D(disparity, 'finald')
+    return True
 
-def SAD(matrix, n):
+def ReadImage(path, xsize, ysize):
+    image = cv2.imread(path, 0)
+    image = cv2.resize(image, dsize=(xsize, ysize), interpolation=cv2.INTER_CUBIC)
+    return image
+
+def LeftShift(matrix, n):
+    shifted = np.zeros(matrix.shape)
+    rows, cols = matrix.shape
+    shifted[:, 0:cols-n] = matrix[:, n:cols]
+    return shifted
+
+def SADValues(matrix, n):
     horz = np.cumsum(matrix, axis=1, dtype=float)
     horz[:, n:] = horz[:, n:] - horz[:, :-n]
     horz = horz[:, n-1:]
@@ -17,48 +27,27 @@ def SAD(matrix, n):
     ret = vert[n-1:, :]
     return ret
 
-def generate_disparity_map(left_path, right_path, name, xsize, ysize, cmp_range, block_size):
+def Disparity(imleft, imright, xsize, ysize, wsize, crange):
+    left = ReadImage(imleft, ysize, xsize)
+    right = ReadImage(imright, ysize, xsize)
+    rows, cols = left.shape
+    matrices = np.zeros(shape=(crange, rows, cols), dtype=float)
+    final = np.zeros(shape=(crange, rows - wsize+1, cols - wsize+1), dtype=float)
+    for n in range(0, crange, 1):
+        matrices[n] = np.abs(right - LeftShift(left, n))
+    for i in range(0, len(matrices), 1):
+        final[i] = SADValues(matrices[i], wsize)
+    disparity = np.argmin(final, 0).astype(np.uint8)
+    return disparity
 
-    gray_left = cv2.imread(left_path, 0)
-    gray_left = cv2.resize(gray_left, dsize=(ysize, xsize), interpolation=cv2.INTER_CUBIC)
+def PlotDepth2D(disparity, name):
+    depth =  (193.001/2) * (3979.911/2) / (disparity + (124.343/2) )
+    colourmap = depth - np.amin(depth)
+    colourmap = colourmap * (255 / np.amax(colourmap))
+    plt.imshow(colourmap, cmap='hsv', interpolation='nearest')
+    plt.colorbar()
+    plt.show()
+    cv2.imwrite(name+'.png', colourmap)
+    return True
 
-    gray_right = cv2.imread(right_path, 0)
-    gray_right = cv2.resize(gray_right, dsize=(ysize, xsize), interpolation=cv2.INTER_CUBIC)
-
-    row_size, col_size = gray_right.shape
-
-    matrices = np.zeros(shape=(cmp_range, row_size, col_size), dtype=float)
-    final = np.zeros(shape=(cmp_range, row_size - block_size+1, col_size - block_size+1), dtype=float)
-
-    for i in range(0, cmp_range):
-        matrices[i] = np.abs(gray_right - left(gray_left, i))
-
-    for i in range(len(matrices)):
-        final[i] = SAD(matrices[i], block_size)
-
-    disparity_matrix = np.argmin(final, 0).astype(np.uint8)
-    
-    #cv2.imwrite(str(name) + '.png', disparity_matrix)
-
-    row, col = disparity_matrix.shape
-
-    # To find depth coordinate --> Z = baseline * ___
-    # We then map the pixels to the range [0, 255]
-
-    depth_map =  ( (193.001/2) * (3979.911/2) / (disparity_matrix + (124.343/2) ) )
-    '''
-    #colourmap = depth_map - np.amin(depth_map)
-    #colourmap = colourmap * (255 / np.amax(colourmap))
-
-    #plt.imshow(colourmap, cmap='hsv', interpolation='nearest')
-    #plt.colorbar()
-    #plt.show()
-    #plt.savefig('new123.png')
-    '''
-    return depth_map
-
-#generate_disparity_map('im0.png', 'im1.png', 'new2', 1000, 1482, 140, 11)
-
-
-
-
+main()
