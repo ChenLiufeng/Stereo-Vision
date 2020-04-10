f464ecbde16eb16e4745d311c3b88145993fe3bf
michaelboyadjian <michael.boyadjian@mail.utoronto.ca>
2020-04-09 22:53:32 -0400

    Final Submission

--- disparity.py
+++ disparity.py
@@ -1,11 +1,16 @@
 import cv2
 import numpy as np
+from scipy import stats 
 import matplotlib.pyplot as plt
 from pfmconversion import *
 
 def main():
-    disparity = Disparity('im0.png', 'im1.png', 'disparity5', 1000, 1482, 5, 140)
-    #PlotDepth2D(disparity, 'depthd')
+    disparity = Disparity('im0_left.png', 'im1_right.png', 'disparitynonoise', 1000, 1482, 11, 140)
+    disparity = Denoising(disparity, 15, 'mean')
+    disparity = Denoising(disparity, 15, 'mode')
+    disparity = np.float32(disparity)
+    save_pfm('disp0.pfm', disparity, scale = 1)
+    PlotDepth2D(disparity, 'depthd')
     return True
 
 def ReadImage(path, xsize, ysize):
@@ -35,20 +40,41 @@ def Disparity(imleft, imright, name, xsize, ysize, wsize, crange):
     rows, cols = left.shape
     matrices = np.zeros(shape=(crange, rows, cols), dtype=float)
     final = np.zeros(shape=(crange, rows - wsize+1, cols - wsize+1), dtype=float)
-    for n in range(0, crange, 1):
+    for n in range(0, crange):
         print(n)
         matrices[n] = np.abs(right - LeftShift(left, n))
     for i in range(0, len(matrices), 1):
         print(i)
         final[i] = SADValues(matrices[i], wsize)
-    disparity = np.argmin(final, 0).astype(np.float32)
-    cv2.imwrite(name+'.png', disparity)
+    disparity = np.argmin(final, 0).astype(np.uint8)
+    disparity = np.float32(disparity)
+    return disparity
+
+def Denoising(disparity, wsize, stat):
+    disparity = disparity - np.amin(disparity)
+    disparity = disparity * (255 / np.amax(disparity))
+    offset = wsize // 2
+    rows, cols = disparity.shape
+    for row in range(offset, rows-offset):
+        for col in range(offset, cols-offset):
+            window = disparity[row-offset:row+offset+1, col-offset:col+offset+1]
+            mean = np.mean(window)
+            mode = np.median(stats.mode(window)[0][0])
+            std = np.std(window)
+            print(std)
+            if stat == 'mean':
+                stat = mean
+            else: 
+                stat = mode
+            if disparity[row, col] > mean+1.5*std or disparity[row, col] < mean-1.5*std :
+                disparity[row:row+2, col:col+3] = stat
     return disparity
 
 def PlotDepth2D(disparity, name):
     depth =  (193.001/4) * (3979.911/4) / (disparity + (124.343/4) )
     colourmap = depth - np.amin(depth)
-    colourmap = colourmap * (255 / np.amax(colourmap))
+    colourmap = colourmap * (350 / np.amax(colourmap))
+    colourmap = colourmap + 1000
     plt.imshow(colourmap, cmap='hsv', interpolation='nearest')
     plt.colorbar()
     plt.show()
